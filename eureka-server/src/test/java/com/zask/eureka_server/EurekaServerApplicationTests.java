cd36fb9f4f3323fb211d77e9886965dffe690096
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-04-21 17:09:18 +0530

    [Bhuvnesh] Feat. Added intital implementation of eureka-server

---
+++ eureka-server/src/test/java/com/zask/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zask.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
